e505cf980bcd6a3df334e3e11ff37f4f09c721ac
zzw <wenge5201314@vip.qq.com>
2019-05-09 22:15:26 +0800

    chessboard

--- chessboard.py
+++ chessboard.py
@@ -10,3 +10,19 @@ GEOMETRY = (15, 15)
 class ChessBoard(object):
     def __init__(self):
         self.__board = [[EMPTY for _ in range(GEOMETRY[0])] for _ in range(GEOMETRY[1])]
+        self.__vector = [[(1, 0), (-1, 0)], [(0, 1), (0, -1)], [(-1, 1), (1, -1)], [(1, 1), (-1, -1)]]
+
+    def wincondition(self, pos_x, pos_y, pieces):
+        current = pieces[pos_x][pos_y]
+        for line in self.__vector:
+            count = 1
+            for direction in line:
+                while True:
+                    tmp_x, tmp_y = pos_x + direction[0], pos_y + direction[1]
+                    if 0 <= tmp_x and tmp_x < 15 and 0 <= tmp_y and tmp_y < 15 and pieces[tmp_x][tmp_y] == current:
+                        count += 1
+                    else:
+                        break
+            if count >= 5:
+                return True
+        return False
